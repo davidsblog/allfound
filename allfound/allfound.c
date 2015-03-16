e9d9458b9479168d40b134b074783675db697991
Davids Blog <david@hobbsindustries.co.uk>
2015-03-16 16:44:02 +0000

    Removed the startup console output.

--- allfound/allfound.c
+++ allfound/allfound.c
@@ -40,7 +40,6 @@ void send_file_response(struct hitArgs *args, char *path, char *request_body, in
 
 int main(int argc, char **argv)
 {
-    puts("sweb server starting\nPress CTRL+C to quit");
     #ifdef __APPLE__
         int PORT = 8181;
         dwebserver(PORT, &send_response, NULL);
@@ -80,6 +79,7 @@ void send_file_response(struct hitArgs *args, char *path, char *request_body, in
         }
     }
     
+    // never send a 404 ... send the homepage instead.
     if (content_type == NULL || (file_id = open(path, O_RDONLY), file_id == -1))
     {
         send_file_response(args, "index.html", request_body, 10);
